bce0a833573c5609592ed6f05ca5dd5f4a2e66e4
Paul Anthony McGowan <xhostcom@gmail.com>
2021-10-02 22:21:45 +0100

    First Commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+    @url = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @output = JSON.parse(@response)
+  end
+
+  def prices
+
   end
 
   def terms
